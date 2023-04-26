19e5b002a4ffa3d9eb371837446ac02b94118234
carlospuigserver <cpuiggut@myuax.com>
2023-04-26 11:04:57 +0200

    probando funcion fumador

--- fumadores.py
+++ fumadores.py
@@ -3,7 +3,7 @@ import random
 import time
 
 ingredientes=["papel","tabaco","filtros","cerillas", "green"]
-fumadores=["1","2","3"]
+fumadores=["1","2","3","4","5"]
 
 
 def agente():
@@ -14,4 +14,19 @@ def agente():
         
         print(f"El agente deja en la mesa {ingrediente1} y {ingrediente2}")
         time.sleep(1)
+
+def fumador(nombre, ingrediente1, ingrediente2):
+    while True:
+        if (ingrediente1 in ingredientes) and (ingrediente2 in ingredientes):
+            ingredientes.remove(ingrediente1)
+            ingredientes.remove(ingrediente2)
+            print(f"Fumador {nombre} esta fumando con {ingrediente1} y {ingrediente2}")
+            time.sleep(2)
+            ingredientes.append(ingrediente1) 
+            ingredientes.append(ingrediente2)
+            print(f"Fumador {nombre} termino de fumar")
+            time.sleep(1)
+        else:
+            print(f"El Fumador {nombre} esta esperando a que el agente defe {ingrediente1} y {ingrediente2} en la mesa")
+            time.sleep(1)
 
